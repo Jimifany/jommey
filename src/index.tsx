9c0b71eddde3773f149db35928811375e6072915
LXY <L3412897394@163.com>
2019-09-15 15:08:48 +0800

    切换皮肤

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
-=======
 import 'antd/dist/antd.css'; 
 import {Provider} from 'mobx-react'
 import * as React from 'react';
@@ -44,4 +31,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
->>>>>>> 全系
